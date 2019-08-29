3d1072c66516f547877b829801a67f62f838075c
SimonLiu <liusihui@outlook.com>
2019-08-29 18:07:30 +0800

    _03_LastLevelToTest

--- Assets/Bottle.cs
+++ Assets/Bottle.cs
@@ -8,16 +8,33 @@ public class Bottle : MonoBehaviour
 
     private int currentAngleIndex;
 
+    private RaycastHit2D hitInfo2D;
+    private int hitMask;
+
 
     void Start()
     {
         currentAngleIndex = 0;
+
+        hitMask += 1 << LayerMask.NameToLayer("Bottle");
     }
 
 
 
     void Update()
     {
-        
+        if (MyClass.currentGameState == GameState.Playing && Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (this.GetComponent<BoxCollider2D>().OverlapPoint(mousePos))
+            {
+                hitInfo2D = Physics2D.Raycast(mousePos, Vector3.forward, Camera.main.farClipPlane);
+                if (hitInfo2D)
+                {
+                    currentAngleIndex = (currentAngleIndex + 1) % angleArray.Length;
+                    this.transform.eulerAngles = Vector3.forward * angleArray[currentAngleIndex];
+                }
+            }
+        }
     }
 }
---
+++ Assets/BottleTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.layer==LayerMask.NameToLayer("PhysicsBall"))
+        {
+            col.transform.position = this.transform.position;
+            col.attachedRigidbody.velocity = 10 * this.transform.up;
+        }
+    }
+}
